f325cbc7ea709c5c1f2c43113382d0aeb36f30f1
cinjude <110071132+cinjude@users.noreply.github.com>
2025-11-10 13:51:40 +0000

    [ad ]

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faClock} from '@fortawesome/free-solid-svg-icons';
 
 
 
 //create your first component
 const SecondsCounter = (props) => {
 	return (
-		<div className="text-center">
+		<div className="container">
 			<div className="counter ">
-				<div>{props.digitSix}</div>
-				<div>{props.digitFive}</div>
-				<div>{props.digitFour}</div>
-				<div>{props.digitThree}</div>
-				<div>{props.digitTwo}</div>
-				<div>{props.digitOne}</div>
+				<div className="fa-style">	<FontAwesomeIcon icon={faClock} /></div>
+				<div className="second">{props.digitSix}</div>
+				<div className="second">{props.digitFive}</div>
+				<div className="second">{props.digitFour}</div>
+				<div className="second">{props.digitThree}</div>
+				<div className="second">{props.digitTwo}</div>
+				<div className="second">{props.digitOne}</div>
 			</div>
 		</div>
 	);
